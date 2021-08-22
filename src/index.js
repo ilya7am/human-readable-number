4fa7364942f9d3ccbc7e3b6d8ffa858f06b83832
ilya7am <88287882+ilya7am@users.noreply.github.com>
2021-08-22 19:56:33 +0400

    docs: update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-function toReadable (number) {
+module.exports = function toReadable(number) {
     let result = '';
     let numb = 0;
   
-      let array1 = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
-    let array2 = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const array1 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const array2 = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
      
         if (number === 0) {
@@ -12,14 +12,14 @@ function toReadable (number) {
 
 
         if (number >= 100) {        
-            numb = (number / 100);
-            result = `${array1[numb]} hundred `;
+            numb = (number / 100) | 0;
+            result = `${array1[numb]} hundred`;
             number = number - numb * 100;
             }
 
 
         if (number > 19) {
-            num = (number / 10);
+            numb = (number / 10) | 0;
             number = number - numb * 10;
             result = `${result} ${array2[numb]} ${array1[number]}`;
         } else {
@@ -30,10 +30,3 @@ function toReadable (number) {
         
     };
     
-
-
-
-  
-
-
-  
